352f994a50de81bc5deb38b770ccedd7560c0171
Alexandru Spinu <Alexandru.Spinu2@endava.com>
2023-04-04 15:45:32 +0300

    Create Test for api with authorization
    - Create Test(create,get,delete and negative) for an api that requires authorization key

---
+++ TestFrame/Fixtures/TheCatApiTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFrame.Fixtures
+{
+    public class TheCatApiTestFixture : RestClientFixture
+    {
+        public string ApiKey { get; set; }
+        public string Api { get; set; }
+        public int VoteId { get; set; }
+    }
+}
---
+++ TestFrame/Models/TheCatApi/GetCatVotesModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TestFrame.Models.TheCatApi
+{
+    public class GetCatVotesModel
+    {
+        public int Id { get; set; }
+        [JsonPropertyName("image_id")]
+        public string ImageId { get; set; }
+
+        [JsonPropertyName("sub_id")]
+        public string SubId { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; }
+
+        public int Value { get; set; }
+        [JsonPropertyName("created_at")]
+        public string CreatedAt { get; set; }
+
+        [JsonPropertyName("country_code")]
+        public string CountryCode { get; set; }
+
+       // public string Image { get; set; }
+    }
+}
