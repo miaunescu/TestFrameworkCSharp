d59571c1a685ee136b0a42962b24c31a4d8e9820
daianatabirca <daiana.tabirca@gmail.com>
2023-04-13 14:32:38 +0300

    - added iteration through JsonObject

--- TestFrame/Tests/CountriesTests/CountriesTest.cs
+++ TestFrame/Tests/CountriesTests/CountriesTest.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using RestSharp;
 using System.Net;
 using System.Security.Authentication;
+using System.Text.Json.Nodes;
 using TestFrame.Base;
 using TestFrame.Builder;
 using TestFrame.Fixtures;
@@ -36,7 +37,53 @@ namespace TestFrame.Tests
                                             .WithRequest($"/name/{TestFixture.Name}", Method.Get)
                                             .Execute<List<CountryModel>>(TestFixture.Client);
             var getResponse = response.Data;
+
             var values = getResponse[0].Currencies.Currency;
+
+            Dictionary<string, string> wrongValuesCurrency = new Dictionary<string, string>
+            {
+            { "name", "Romanian leuuuuu" },
+            { "symbol", "leiiiii" }
+            };
+
+            var iddCountry = new IddModel()
+            {
+                Root = "+4",
+                Suffixes = new List<string> { "0" }
+            };
+
+            var country = new CountryModel()
+            { 
+                Cca2 = "gkjsdkjfsdkfjs",
+                Cca3 = "sdfsdfsdfs"
+            };
+
+
+            #region Asserts
+            using (new AssertionScope())
+            {
+                //getResponse[0].Idd.Suffixes[0].Should().Be(iddCountry.Suffixes[0]);
+                //getResponse[0].Cca3.Should().NotBe(country.Cca2);
+                //getResponse[0].Cca3.Should().Be(country.Cca2);
+
+                //getResponse[0].Idd.Suffixes.Should().BeEquivalentTo(iddCountry.Suffixes);
+
+                foreach (var currency in values)
+                {
+                    JsonObject valueCurrency = (JsonObject)currency.Value;
+
+                    foreach (var key in wrongValuesCurrency.Keys)
+                    {
+                        if (valueCurrency.ContainsKey(key))
+                        {
+                            wrongValuesCurrency[key].Should().Be((string)valueCurrency[key]);
+                        }
+                    }
+                }
+
+            }
+            #endregion
+            
         }
 
     }
