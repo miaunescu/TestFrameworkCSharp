9b08f4c7e8fba95159597a12163f2ae5a761e5b1
Alexandru Spinu <Alexandru.Spinu2@endava.com>
2023-04-19 17:14:21 +0300

    Create setup for database connection and tests
    - Create structure for database connection with dapper
    - Create DB tests

---
+++ TestFrame/Databases/NorthwindDatabase.cs
@@ -0,0 +1,64 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestFrame.Models.Northwind;
+
+namespace TestFrame.Databases
+{
+    public class NorthwindDatabase : BaseDatabasesConfiguration
+    {
+        private IDbConnection dbConnection;
+
+        public NorthwindDatabase()
+        {
+            dbConnection = new SqlConnection(config.GetConnectionString("Northwind"));
+        }
+
+        public List<NorthwindCustomersModel> GetClients()
+        {
+            var query = "SELECT TOP 10 * FROM dbo.Customers";
+            return dbConnection.Query<NorthwindCustomersModel>(query).ToList();
+        }
+
+        public NorthwindCustomersModel AddCustomer(NorthwindCustomersModel customerModel)
+        {
+
+
+            var query = "INSERT INTO dbo.Customers(CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax)" +
+                "VALUES (@CustomerID, @CompanyName, @ContactName, @ContactTitle, @Address, @City, @Region, @PostalCode, @Country, @Phone, @Fax);" +
+                "SELECT CustomerId FROM dbo.Customers WHERE CustomerID = @CustomerID;";
+
+            var customerId = dbConnection.Query<string>(query, new
+            {
+                @CustomerId = customerModel.CustomerID,
+                @CompanyName = customerModel.CompanyName,
+                @ContactName = customerModel.ContactName,
+                @ContactTitle = customerModel.ContactTitle,
+                @Address = customerModel.Address,
+                @City = customerModel.City,
+                @Region = customerModel.Region,
+                @PostalCode = customerModel.PostalCode,
+                @Country = customerModel.Country,
+                @Phone = customerModel.Phone,
+                @Fax = customerModel.Fax,
+            }).Single();
+
+            customerModel.CustomerID = customerId;
+            return customerModel;
+        }
+
+        public void DeleteCustomer(string customerId)
+        {
+            var query = "DELETE FROM dbo.Customers where CustomerID = @CustomerID";
+
+            dbConnection.Execute(query, new { customerId });
+        }
+    }
+}
+
---
+++ TestFrame/Tests/NorthwindDatabaseTests/NorthwindCustomersDatabaseTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestFrame.Databases;
+using TestFrame.Models.Northwind;
+using Xunit;
+
+namespace TestFrame.Tests.NorthwindDatabaseTests
+{
+    //You need to start the server 3.70.90.215 before running the tests
+    public class NorthwindCustomersDatabaseTests
+    {
+        NorthwindDatabase northwindDatabase;
+
+        public NorthwindCustomersDatabaseTests()
+        {
+            northwindDatabase = new();
+        }
+
+        [Fact]
+        public void Get_Customers_Northwind_Database_Test()
+        {
+            var firstClient = northwindDatabase.GetClients().FirstOrDefault();
+
+            using (new AssertionScope())
+            {
+                firstClient.CompanyName.Should().Be("Alfreds Futterkiste");
+            }
+        }
+
+        [Fact]
+        public void Add_Customer_Northwind_Database_Test()
+        {
+            NorthwindCustomersModel model = new()
+            {
+                CustomerID = "AUTO1",
+                CompanyName = "AutoCompany1",
+                ContactName = "ContactAuto1",
+                ContactTitle = "ContactTitleAuto1",
+                Address = "Endava County 1 Ave",
+                City = "Bucharest",
+                Region = "Tineretului",
+                PostalCode = "12345",
+                Country = "Romania",
+                Phone = "898989",
+                Fax = "000-000"
+            };
+
+            var client = northwindDatabase.AddCustomer(model);
+
+            using (new AssertionScope())
+            {
+                client.Country.Should().Be(model.Country);
+                client.CustomerID.Should().Be(model.CustomerID);
+                client.CompanyName.Should().Be(model.CompanyName);
+                client.ContactName.Should().Be(model.ContactName);
+                client.ContactTitle.Should().Be(model.ContactTitle);
+                client.Address.Should().Be(model.Address);
+                client.City.Should().Be(model.City);
+                client.Region.Should().Be(model.Region);
+                client.PostalCode.Should().Be(model.PostalCode);
+                client.Country.Should().Be(model.Country);
+                client.Phone.Should().Be(model.Phone);
+                client.Fax.Should().Be(model.Fax);
+            }
+
+            //Delete the customer after creation, to be able to create another one with the same CustomerID
+            northwindDatabase.DeleteCustomer(model.CustomerID);
+        }
+    }
+}
+
