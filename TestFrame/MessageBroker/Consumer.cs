452dc066701dd2990fe9fadae3bd38d2b9b2ca0a
virgildimitriu <virgildimitriu@yahoo.com>
2023-05-05 16:56:05 +0300

    Added consumer and producer classed

--- TestFrame/MessageBroker/Consumer.cs
+++ TestFrame/MessageBroker/Consumer.cs
@@ -1,59 +1,32 @@
-﻿using System;
-using System.Threading.Tasks;
-using MassTransit;
-using Newtonsoft.Json;
-using RestSharp;
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-namespace ConsumerApp
+var factoryConsumer = new ConnectionFactory { HostName = "35.158.22.93", Port = 5672 };
+using var connectionConsumer = factoryConsumer.CreateConnection();
+using var channelConsumer = connectionConsumer.CreateModel();
+
+channelConsumer.QueueDeclare(queue: "RestCountries",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channelConsumer);
+consumer.Received += (model, ea) =>
 {
-    public class CountryModel
-    {
-        public string Name { get; set; }
-        public string Capital { get; set; }
-        public string Region { get; set; }
-        public string Subregion { get; set; }
-        public int Population { get; set; }
-    }
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+channelConsumer.BasicConsume(queue: "RestCountries",
+                     autoAck: true,
+                     consumer: consumer);
 
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
-            {
-                cfg.Host(new Uri("rabbitmq://35.158.22.93"), h =>
-                {
-                    h.Username("guest");
-                    h.Password("guest");
-                });
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
 
-                cfg.ReceiveEndpoint("RestCountries", e =>
-                {
-                    e.Handler<CountryModel>(async context =>
-                    {
-                        var country = context.Message;
-                        var client = new RestClient($"https://restcountries.com/v3.1/name/{country.Name}");
-                        var request = new RestRequest(Method.GET);
-                        var response = await client.ExecuteAsync(request);
-                        var content = response.Content;
-                        var details = JsonConvert.DeserializeObject<CountryModel[]>(content);
 
-                        Console.WriteLine($"Name: {details[0].Name}");
-                        Console.WriteLine($"Capital: {details[0].Capital}");
-                        Console.WriteLine($"Region: {details[0].Region}");
-                        Console.WriteLine($"Subregion: {details[0].Subregion}");
-                        Console.WriteLine($"Population: {details[0].Population}");
-                        Console.WriteLine();
-                    });
-                });
-            });
 
-            await bus.StartAsync();
-
-            Console.WriteLine("Listening for messages.. Press any key to exit");
-            Console.ReadKey();
-
-            await bus.StopAsync();
-        }
-    }
-}
--- TestFrame/MessageBroker/Producer.cs
+++ TestFrame/MessageBroker/Producer.cs
@@ -1,21 +1,20 @@
 ﻿//using System.Text;
 //using RabbitMQ.Client;
 
-//var factory = new ConnectionFactory { HostName = "35.158.22.93", Port=5672 };
-//using var connection = factory.CreateConnection();
-//using var channel = connection.CreateModel();
+//var factoryProducer = new ConnectionFactory { HostName = "35.158.22.93", Port = 5672 };
+//using var connectionProducer = factoryProducer.CreateConnection();
+//using var channelProducer = connectionProducer.CreateModel();
 
-//channel.QueueDeclare(queue: "RestCountries",
+//channelProducer.QueueDeclare(queue: "RestCountries",
 //                     durable: false,
 //                     exclusive: false,
 //                     autoDelete: false,
 //                     arguments: null);
 
-//const string message = "romania";
-
+//const string message = "Romania";
 //var body = Encoding.UTF8.GetBytes(message);
 
-//channel.BasicPublish(exchange: string.Empty,
+//channelProducer.BasicPublish(exchange: string.Empty,
 //                     routingKey: "RestCountries",
 //                     basicProperties: null,
 //                     body: body);
@@ -23,51 +22,3 @@
 
 //Console.WriteLine(" Press [enter] to exit.");
 //Console.ReadLine();
-
-
-
-//===============================================================
-//===============================================================
-//===============================================================
-
-using System.Text;
-using MassTransit;
-using RabbitMQ.Client;
-using RestSharp;
-
-var factory = new ConnectionFactory { HostName = "35.158.22.93", Port = 5672 };
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
-
-channel.QueueDeclare(queue: "RestCountries",
-                     durable: false,
-                     exclusive: false,
-                     autoDelete: false,
-                     arguments: null);
-
-var message = GetMessage(args);
-var body = Encoding.UTF8.GetBytes(message);
-
-var properties = channel.CreateBasicProperties();
-properties.Persistent = true;
-
-channel.BasicPublish(exchange: string.Empty,
-                     routingKey: "RestCountries",
-                     basicProperties: properties,
-                     body: body);
-Console.WriteLine($" [x] Sent {message}");
-
-Console.WriteLine(" Press [enter] to exit.");
-Console.ReadLine();
-
-static string GetMessage(string[] args)
-{
-    return ((args.Length > 0) ? string.Join(" ", args) : "Romania!");
-}
-
-
-//===============================================================
-//===============================================================
-//===============================================================
-
-
